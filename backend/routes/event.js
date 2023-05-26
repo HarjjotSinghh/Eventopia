2e011bb8a83fd12dc885c16499f316660cbe19eb
Ansh-Rajput <anshravikrish425317@gmail.com>
2023-05-26 19:45:35 +0530

    added Update event route.

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -9,9 +9,9 @@ router.get("/getevent",async (req,res)=>{//This route gives data of all Events.
     res.send(data);
 })
 router.post("/addevent",[//This route add Events.
-    body("eventDetails").notEmpty(),
-    body("organizer").notEmpty(),
-    body("management").notEmpty()
+body("eventDetails").notEmpty(),
+body("organizer").notEmpty(),
+body("management").notEmpty()
 ],async (req,res)=>{
     // URL:http://localhost:5000/api/event/addevent
     const result = validationResult(req);
@@ -33,6 +33,43 @@ router.post("/addevent",[//This route add Events.
     }});
 
     data.save();
+
+    res.send(data);
+    }
+    else{
+        res.send({errors: result.array()});
+    } 
+//     sample data of Body : {
+//   "eventDetails":{"title":"Example1","date":"DAte","timing":"timing","venue":"venue","description":"desc","poster":"poster"},
+//   "organizer":{"society":"socity","socialmedia":"media","website":"website"},
+//   "management":{"email":"email","contact":846456546513218}
+// }
+})
+
+router.put("/updateevent/:id",[//This route will Update Events.
+    body("eventDetails").notEmpty(),
+    body("organizer").notEmpty(),
+    body("management").notEmpty()
+],async (req,res)=>{
+    // URL:http://localhost:5000/api/event/updateevent/646d0350f84794410e70fbfa
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        const data = await eventModal.findByIdAndUpdate(req.params.id,{eventDetails:{
+            title:req.body.eventDetails.title,
+        date:req.body.eventDetails.date,
+        timing:req.body.eventDetails.timing,
+        venue:req.body.eventDetails.venue,
+        description:req.body.eventDetails.description,
+        poster:req.body.eventDetails.poster
+        },organizer:{
+            society:req.body.organizer.society,
+            socialmedia:req.body.organizer.socialmedia,
+            website:req.body.organizer.website
+        },management:{
+            email:req.body.management.email,
+            contact:req.body.management.contact
+        }});
+    // data.save();
 
     res.send(data);
     }
