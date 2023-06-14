78e5c3b6c0f91b461ca05985a1cd797325f3bc8b
Harjot Singh <harjjotsinghh@gmail.com>
2023-06-14 13:13:32 +0530

    Final changes before project submission

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import dataProvider from './dataProvider';
+// import React from 'react';
+// import { Admin, Resource } from 'react-admin';
+// import dataProvider from './dataProvider';
 
-// Import your resources (collections) components
-import { PostList, PostEdit, PostCreate } from './resources/Post';
+// // Import your resources (collections) components
+// import { PostList, PostEdit, PostCreate } from './resources/Post';
 
-const Dashboard = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource
-            name="posts"
-            list={PostList}
-            edit={PostEdit}
-            create={PostCreate}
-        />
-        {/* Add more resources (collections) as needed */}
-    </Admin>
-);
+// const Dashboard = () => (
+//     <Admin dataProvider={dataProvider}>
+//         <Resource
+//             name="posts"
+//             list={PostList}
+//             edit={PostEdit}
+//             create={PostCreate}
+//         />
+//         {/* Add more resources (collections) as needed */}
+//     </Admin>
+// );
 
-export default Dashboard;
+// export default Dashboard;
--- frontend/src/dataProvider.js
+++ frontend/src/dataProvider.js
@@ -1,18 +1,18 @@
-import { fetchUtils } from 'react-admin';
+// import { fetchUtils } from 'react-admin';
 
-const apiUrl = 'http://localhost:5000';
+// const apiUrl = 'http://localhost:5000';
 
-const httpClient = fetchUtils.fetchJson;
+// const httpClient = fetchUtils.fetchJson;
 
-const dataProvider = {
-    getList: async (resource, params) => {
-        const url = `${apiUrl}/${resource}`;
-        const response = await httpClient(url);
-        return {
-            data: response.json,
-            total: response.headers.get('X-Total-Count'),
-        };
-    },
-};
+// const dataProvider = {
+//     getList: async (resource, params) => {
+//         const url = `${apiUrl}/${resource}`;
+//         const response = await httpClient(url);
+//         return {
+//             data: response.json,
+//             total: response.headers.get('X-Total-Count'),
+//         };
+//     },
+// };
 
-export default dataProvider;
+// export default dataProvider;
